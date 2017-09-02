87ede65fb8dafe690a74995bfb651c01e9d6a35b
rusline18 <absaruslan90@mail.ru>
2017-09-02 12:01:16 +0300

    completed the task

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const readline = require('readline');
 const ansi = require('ansi');
 const cursor = ansi(process.stdout);
-cursor.beep();
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+rl.question('node js это круто? ', (answer) => {
+	switch (answer.toLowerCase()){
+		case 'да':
+		case 'yes':
+			cursor.green();
+			console.log(answer, '- это правильный ответ');
+			cursor.fg.reset();
+			break;
+		default:
+			cursor.beep();
+			cursor.red();
+			console.log(answer, '-ошибочное мнение');
+			cursor.fg.reset();
+			console.log('Попробуйте еще раз');
+	}
+	rl.close();
+} );
+rl.write('Да');
