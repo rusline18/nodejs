da756170b201ddccaba64d68f9f20199356e7f70
Ruslan Absalyamov <kazan1811>
2017-09-07 18:22:10 +0300

    Поменял вместо ковычек на "/"

--- analyz.js
+++ analyz.js
@@ -5,8 +5,8 @@ fs.readFile('log.txt', function(err, data){
 	if(err) throw err;
 	var p = data.toString().split('\n');
 	var count = p.length;
-	var win = p.count('true');
-	var lost = p.count('false');
+	var win = p.count(/true/);
+	var lost = p.count(/false/);
 	console.log('Количество игр было: '+ count);
 	console.log('Выйграно: '+ win +' игр, проигранных: '+ lost);
 });
