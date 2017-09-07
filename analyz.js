622cee11d91b76d0b6149cb43c6352a8ce6bbda6
Ruslan Absalyamov <kazan1811>
2017-09-07 15:53:47 +0300

    Программа анализа игры

---
+++ analyz.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+require('sugar')();
+
+	fs.readFile('log.txt', function(err, data){
+		if(err) throw err;
+		var p = data.toString().split('\n');
+		var count = p.length;
+		var win = p.count('true');
+		var lost = p.count('false');
+		console.log('Количество игр было: '+ count);
+		console.log('Выйграно: '+ win +' игр, проигранных: '+ lost);
+	});
