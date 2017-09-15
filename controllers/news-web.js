7dc55bcf826ab97bb6bcf83f465187f13a7c3676
Ruslan Absalyamov <kazan1811>
2017-09-15 16:37:52 +0300

    Без комментов

--- controllers/news-web.js
+++ controllers/news-web.js
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/../views');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 request('https://nix-tips.ru/tag/yii2', function (err, respons, html) {
     if (!err && respons.statusCode === 200){
@@ -19,23 +19,37 @@ request('https://nix-tips.ru/tag/yii2', function (err, respons, html) {
         $('article').each(function(i, element){
             let title = $(this).find('.entry-title');
             let date = $(this).find('.entry-date');
-            let content = $(this).find('p');
-            let arr = [];
-            while (date < 50){
-                arr.add(date);
-            }
-            app.get('/', function(req, res){
+            let post = $(this).find('p');
+            let results = [];
+            app.post('/', function (req, res) {
+                for (let i=1; i <= req.body.posts; i++){
+                    results.push({
+                        title: $(title).text(),
+                        date: $(date).text(),
+                        post: $(post).text(),
+                    });
+                }
+                console.log(results);
                 res.render('main', {
-                    header: 'Новости по yii2',
-                    date: $(date).text(),
-                    title: $(title).text(),
-                    content: $(content).text(),
+                    header: 'Новости по ii2',
+                    content: 'Выберите сколько статей нужно отобразить',
+                    title: results.title,
+                    date: results.date,
+                    post: results.post,
                 });
             });
         });
     } else {
         console.log('Произошла ошибка!');
     }
+});
+
+
+app.get('/', function(req, res){
+    res.render('main', {
+        header: 'Новости по yii2',
+        content: 'Выберите сколько статей нужно отобразить?'
+    });
 });
 
 app.listen(8888, function () {
