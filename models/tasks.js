b70ba010c501a256490fa90928d6da72d27e860e
rusline18 <absaruslan90@mail.ru>
2017-09-17 20:18:26 +0300

    Модель для задачника

---
+++ models/tasks.js
@@ -0,0 +1,25 @@
+let pool = require('config.js');
+
+let Tasks = {
+	create: function(callback){
+		//TODO
+	},
+
+	delet: function(id, callback){
+		//TODO
+	},
+
+	update: function(id, callback){
+		//TODO
+	},
+
+	prioritet: function(id, callback){
+		//TODO
+	},
+
+	list: function(callback){
+		//TODO
+	}
+};
+
+module.exports = Tasks;
