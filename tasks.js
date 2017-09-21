0a205d36be8236070e11f7aa88e9c88020f9148d
Ruslan Absalyamov <kazan1811>
2017-09-21 16:01:18 +0300

    no comment

--- tasks.js
+++ tasks.js
@@ -8,8 +8,6 @@ app.engine('hbs', templating.handlebars);
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
-
-//Это правильно вывожу и в шаблоне скорее всего правильно буду выводить?
 app.get('/', function (req, res) {
     Task.list(task => {
         res.render('main', task)
