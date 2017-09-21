ac08ad92f555ae5d5a0d99ae217ca49730653f06
Ruslan Absalyamov <kazan1811>
2017-09-21 09:42:32 +0300

    Убрал конфликты

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 let mysql = require('mysql');
 
-<<<<<<< HEAD
 let config = mysql.createPool({
     host		: '127.0.0.1',
     user		: 'root',
@@ -9,17 +8,3 @@ let config = mysql.createPool({
 });
 
 module.exports = config;
-=======
-let pool = mysql.createPool({
-	host		: '127.0.0.1',
-	user		: 'root',
-	password	: null,
-	database	: 'tasks'
-});
-
-pool.connect(function(err){
-	if (err) {
-		console.error(err);
-	};
-});
->>>>>>> 06c0e358c6542b743492ff9ffbb8e31e92b8010c
--- models/tasks.js
+++ models/tasks.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let pool = require('../config/config');
 
 let Tasks = {
@@ -78,55 +77,12 @@ let Tasks = {
                             return;
                         }
 
-                        resolve(rows);
+                        resolve(rows);//Здесь надо так выводить или как-то по другому?
                         pool.release();
                     });
             });
         });
     }
-=======
-let pool = require(__dirname + '/../config.js');
-
-let Tasks = {
-	create: function(callback){
-		pool.getConnection(function(err, connection){
-			pool.query('INSERT INTO tasks SET ?'
-				post,
-				function(err, rows){
-				pool.release();
-			});
-		});
-	},
-
-	delet: function(id, callback){
-		pool.getConnection(function(err, connection){
-			pool.query('UPDATE INTO tasks SET id = ?'
-				[id],
-				function(err, rows){
-				pool.release();
-			});
-		});
-	},
-
-	update: function(id, callback){
-		pool.getConnection(function(err, connection){
-			pool.query('UPDATE INTO tasks SET id = ?'
-				post,
-				function(err, rows){
-				pool.release();
-			});
-		});
-	},
-
-	list: function(callback){
-		pool.getConnection(function(err, connection){
-			pool.query('SELECT * FROM tasks',
-				function(err, rows){
-				pool.release();
-			});
-		});
-	}
->>>>>>> 06c0e358c6542b743492ff9ffbb8e31e92b8010c
 };
 
 module.exports = Tasks;
