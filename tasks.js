6ecd37b22c5d46007e34db4c6741057bdf6689a3
Ruslan Absalyamov <kazan1811>
2017-09-22 18:09:28 +0300

    Отобразил анные в правильном формате

--- tasks.js
+++ tasks.js
@@ -6,11 +6,21 @@ let express = require('express'),
 
 
 app.engine('hbs', templating.handlebars);
+app.use(express.static(__dirname));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
     Task.list().then(tasks => {
+        tasks = tasks.map(function (task) {
+            moment.locale('ru');
+            moment.updateLocale('ru', {
+                months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+            });
+            task.srok = moment(task.srok).format('D MMMM YYYY HH:mm');
+
+            return task;
+        });
         res.render('main', {tasks})
     })
 });
