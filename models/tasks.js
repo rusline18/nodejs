214736946873725df9e29d0e9d248efb476e3d96
Ruslan Absalyamov <kazan1811>
2017-09-22 15:48:44 +0300

    изменил и убрал все времено другие элементы модли

--- models/tasks.js
+++ models/tasks.js
@@ -2,103 +2,22 @@ let pool = require('../config/config');
 global.Promise = require('bluebird');
 
 let Tasks = {
-    create: function(){
-        return new Promise((resolve, reject) => {
-            pool.getConnection(function(err, connection){
-                if (err) reject(err);
-                return;
-
-                connection.query('INSERT INTO tasks SET ?',
-                    post,
-                    function(err, rows){
-                        if (err || !rows){
-                            reject(err);
-                            return;
-                        }
-
-                        resolve(rows);
-                        pool.release();
-                    });
-            });
-        }).then(
-            error => {
-                console.log(error);
-            }
-        );
-    },
-
-    delet: function(id){
-        return new Promise((resolve, reject) => {
-            pool.getConnection(function(err, connection){
-                if (err) reject(err);
-                return;
-
-                connection.query('DELETE INTO tasks SET ?',
-                    post,
-                    function(err, rows){
-                        if (err || !rows){
-                            reject(err);
-                            return;
-                        }
-
-                        resolve(rows);
-                        pool.release();
-                    });
-            });
-        }).then(
-            error => {
-                console.log(error);
-            }
-        );
-    },
-
-    update: function(id){
-        return new Promise((resolve, reject) => {
-            pool.getConnection(function(err, connection){
-                if (err) reject(err);
-                return;
-
-                connection.query('UPDATE INTO tasks SET ?',
-                    post,
-                    function(err, rows){
-                        if (err || !rows){
-                            reject(err);
-                            return;
-                        }
-
-                        resolve(rows);
-                        pool.release();
-                    });
-            });
-        }).then(
-            error => {
-                console.log(error);
-            }
-        );
-    },
-
     list: function(){
         return new Promise((resolve, reject) => {
             pool.getConnection(function(err, connection){
                 if (err) console.log(reject(err));
-                return;
 
                 connection.query('SELECT * FROM tasks',
                     function(err, rows){
                         if (err || !rows){
                             reject(err);
-                            return;
                         }
 
-                        resolve(rows);//Здесь надо так выводить или как-то по другому?
-                        pool.release();
+                        resolve(rows);
+                        connection.release();
                     });
             });
-        }).then(
-            error => {
-                console.log(error);
-            }
-        );
+        })
     }
 };
 
